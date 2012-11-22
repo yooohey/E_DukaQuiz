ea2149b958601cab22f50027e9b988af7484756c
suwala <aoisaoais@gmail.com>
2012-11-22 16:35:28 +0900

    style change

--- gen/sample/edukaquiz/R.java
+++ gen/sample/edukaquiz/R.java
@@ -107,6 +107,7 @@ public final class R {
     }
     public static final class style {
         public static final int AppTheme=0x7f070000;
+        public static final int original=0x7f070001;
     }
     public static final class xml {
         public static final int prefs=0x7f040000;
